a5e1606c688190b1c5e11a0cef31acb10963656b
Pankaj Parihar <pnkjparihar38@gmail.com>
2024-07-12 23:14:25 +0530

    Link updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
       );
 
       const data = await response.json();
-      console.log(data);
+      console.log("ATLAS" + data);
       if (response.ok) {
         toast.success("Logged In");
         dispatch(bagAction.addAll({ id: data.user.bag }));
